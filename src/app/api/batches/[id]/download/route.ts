ebdd11f56b65d2c33e3273c933fcc5129ce4569e
Masumi Mutsuda <mutsuda@gmail.com>
2025-07-17 22:24:37 +0200

    fix: use @prisma/client in all API routes (remove generated import)

--- src/app/api/batches/[id]/download/route.ts
+++ src/app/api/batches/[id]/download/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "../../../../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import authOptions from "@/lib/authOptions";
 import { PDFDocument, rgb } from "pdf-lib";
